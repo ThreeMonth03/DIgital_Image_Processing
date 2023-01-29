4b0408c9d1a501536168c46f9c98e18f89a3bd38
threemonth3 <sankagetsu.cs08@nycu.edu.tw>
2023-01-29 22:47:42 +0800

    write_part_BMP.hpp

---
+++ Homework_1/BMP.hpp
@@ -0,0 +1,21 @@
+#pragma pack(2)//Reduce the blank between the variable.
+struct BmpFileHeader{
+    uint16_t bfTybe = 0x424D;//Check if it is {'B','M'}
+    uint32_t bfSize;//The size of BMP_file
+    uint16_t bfReserved1 = 0;
+    uint16_t bfReserved2 = 0;
+    uint32_t bfOffBits = 54;//14(BmpFileHeader)+40(BmpInfoHeader) If it is grayscale image, adding extra 1024(調色盤)
+};
+struct BmpInfoHeader{
+    uint32_t biSize = 40;//The size of BmpInfoHeader
+    uint32_t biWidth;//The width of the picture
+    uint32_t biHeight;//The height of the picture
+    uint16_t biPlanes = 1; // 1=defeaul, 0=custom
+    uint16_t biBitCount;//RGB=24,
+    uint32_t biCompression = 0;
+    uint32_t biSizeImage = 0;//The number of the pixel(RGB*3)
+    uint32_t biXPelsPerMeter = 0; // 72dpi=2835, 96dpi=3780
+    uint32_t biYPelsPerMeter = 0; // 120dpi=4724, 300dpi=11811
+    uint32_t biClrUsed = 0;
+    uint32_t biClrImportant = 0;
+};
---
+++ Homework_1/ImgRWbmp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BMP.hpp"
+
+
+int main(){
+    BmpFileHeader a;
+    std::cout<<a.bfTybe<<std::endl;
+    std::cout<<2<<std::endl;
+    return 0;
+}

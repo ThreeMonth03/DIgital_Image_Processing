cd4a249b677cf018de94145617ca823aa6aaf1a7
ThreeMonth03 <sankagetsu.cs08@nycu.edu.tw>
2023-01-29 23:36:27 +0800

    write-part-BMP.hpp

--- Homework_1/BMP.hpp
+++ Homework_1/BMP.hpp
@@ -4,7 +4,7 @@ struct BmpFileHeader{
     uint32_t bfSize;//The size of BMP_file
     uint16_t bfReserved1 = 0;
     uint16_t bfReserved2 = 0;
-    uint32_t bfOffBits = 54;//14(BmpFileHeader)+40(BmpInfoHeader) If it is grayscale image, adding extra 1024(調色盤)
+    uint32_t bfOffBits = 54;//14(BmpFileHeader)+40(BmpInfoHeader) If it is grayscale image, adding extra 1024(Palette)
 };
 struct BmpInfoHeader{
     uint32_t biSize = 40;//The size of BmpInfoHeader
@@ -16,6 +16,18 @@ struct BmpInfoHeader{
     uint32_t biSizeImage = 0;//The number of the pixel(RGB*3)
     uint32_t biXPelsPerMeter = 0; // 72dpi=2835, 96dpi=3780
     uint32_t biYPelsPerMeter = 0; // 120dpi=4724, 300dpi=11811
-    uint32_t biClrUsed = 0;
+    uint32_t biClrUsed = 0;//The number of the color in the Palette
     uint32_t biClrImportant = 0;
 };
+#pragma pack() 
+
+struct basic_ImgData{
+    std::vector<unsigned char> raw_img;
+	uint32_t width;
+	uint32_t height;
+	uint16_t bits;
+};
+
+class ImgData: public basic_ImgData {
+
+}
